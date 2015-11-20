a9473947cb666dd3bb1dc6b812208c8ec6530bcb
Martin Cosgrave <martin@ciderpunk.com>
2015-11-20 18:12:43 +0000

    move downloaded files around

--- solr/get.sh
+++ solr/get.sh
@@ -1,2 +1,5 @@
 cd "`dirname $0`"
-wget -c -O solr4.war https://artifacts.alfresco.com/nexus/service/local/repo_groups/public/content/org/alfresco/alfresco-solr4/5.0.d/alfresco-solr4-5.0.d-ssl.war
+
+mkdir -p files/tmp
+
+wget -c -O files/tmp/solr4.war https://artifacts.alfresco.com/nexus/service/local/repo_groups/public/content/org/alfresco/alfresco-solr4/5.0.d/alfresco-solr4-5.0.d-ssl.war
