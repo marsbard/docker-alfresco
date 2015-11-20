744315602e5234be81a7409785a4a1c203b684b7
Martin Cosgrave <martin@ciderpunk.com>
2015-11-20 18:22:38 +0000

    add share

--- share/get.sh
+++ share/get.sh
@@ -1,3 +1,5 @@
 cd "`dirname $0`"
-wget -c -O share.war https://artifacts.alfresco.com/nexus/service/local/repo_groups/public/content/org/alfresco/share/5.0.d/share-5.0.d.war
+
+mkdir -p files/tmp
+wget -c -O files/tmp/share.war https://artifacts.alfresco.com/nexus/service/local/repo_groups/public/content/org/alfresco/share/5.0.d/share-5.0.d.war
 
