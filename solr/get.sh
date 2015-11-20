4848b497eaa5134d6ff7b68157e87c2ee747854b
Martin Cosgrave <martin@ciderpunk.com>
2015-11-20 19:29:14 +0000

    solr config

--- solr/get.sh
+++ solr/get.sh
@@ -3,3 +3,6 @@ cd "`dirname $0`"
 mkdir -p files/tmp
 
 wget -c -O files/tmp/solr4.war https://artifacts.alfresco.com/nexus/service/local/repo_groups/public/content/org/alfresco/alfresco-solr4/5.0.d/alfresco-solr4-5.0.d-ssl.war
+
+wget -c -O files/tmp/alfresco-solr4-config-ssl.zip https://artifacts.alfresco.com/nexus/service/local/repo_groups/public/content/org/alfresco/alfresco-solr4/5.0.d/alfresco-solr4-5.0.d-config-ssl.zip
+
