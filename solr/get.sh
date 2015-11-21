50dabece31f95a8f7c00d59b766c5f04222c6840
Martin Cosgrave <martin@ciderpunk.com>
2015-11-21 17:12:38 +0000

    add tomcat config and keystore same as other tomcats

--- solr/get.sh
+++ solr/get.sh
@@ -6,3 +6,11 @@ wget -c -O files/tmp/solr4.war https://artifacts.alfresco.com/nexus/service/loca
 
 wget -c -O files/tmp/alfresco-solr4-config-ssl.zip https://artifacts.alfresco.com/nexus/service/local/repo_groups/public/content/org/alfresco/alfresco-solr4/5.0.d/alfresco-solr4-5.0.d-config-ssl.zip
 
+
+
+for file in browser.p12 generate_keystores.sh keystore keystore-passwords.properties \
+	ssl-keystore-passwords.properties ssl-truststore-passwords.properties ssl.keystore ssl.truststore
+do
+  wget -c -O files/tmp/keystore/$file http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD/root/projects/repository/config/alfresco/keystore/$file
+done
+
