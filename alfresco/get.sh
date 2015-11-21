34b1ef1996750a355d4aace2831653f76a502115
Martin Cosgrave <martin@ciderpunk.com>
2015-11-21 11:01:15 +0000

    switch to mysql

--- alfresco/get.sh
+++ alfresco/get.sh
@@ -5,6 +5,13 @@ wget -c -O files/tmp/alfresco.war https://artifacts.alfresco.com/nexus/service/l
 
 wget -c https://jdbc.postgresql.org/download/postgresql-9.4-1205.jdbc42.jar -O files/tmp/postgresql.jdbc.jar
 
+wget -c -O files/tmp/mysql.jdbc.zip http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.37.zip
+pushd files/tmp
+unzip mysql.jdbc.zip  mysql-connector-java-5.1.37/mysql-connector-java-5.1.37-bin.jar
+cp mysql-connector-java-5.1.37/mysql-connector-java-5.1.37-bin.jar ./mysql.jdbc.jar
+rm -rf mysql-connector-java-5.1.37
+popd
+
 
 for file in browser.p12 generate_keystores.sh keystore keystore-passwords.properties \
 	ssl-keystore-passwords.properties ssl-truststore-passwords.properties ssl.keystore ssl.truststore
