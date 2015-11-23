5b97027623cfb963ce6a27d56c9379acda4b26c8
Martin Cosgrave <martin@ciderpunk.com>
2015-11-23 10:19:31 +0000

    fix path creation

--- solr/get.sh
+++ solr/get.sh
@@ -1,6 +1,6 @@
 cd "`dirname $0`"
 
-mkdir -p files/tmp
+mkdir -p files/tmp/keystore
 
 wget -c -O files/tmp/solr4.war https://artifacts.alfresco.com/nexus/service/local/repo_groups/public/content/org/alfresco/alfresco-solr4/5.0.d/alfresco-solr4-5.0.d-ssl.war
 
