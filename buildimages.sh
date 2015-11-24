9bbc645bf71172f63f9f499940946ab67de3e2f9
Martin Cosgrave <martin@ciderpunk.com>
2015-11-24 11:17:59 +0000

    more dns

--- buildimages.sh
+++ buildimages.sh
@@ -2,5 +2,5 @@
 
 cd "`dirname $0`"
 
-docker build -f tomcat/Dockerfile.dev -t marsbard/tomcat ./tomcat
+docker build -t marsbard/tomcat ./tomcat
 #docker build -t marsbard/registrator:latest ./consul/registrator
--- tomcat/build-dev.sh
+++
@@ -1,2 +0,0 @@
-cd "`dirname $0`"
-docker build -f Dockerfile.dev -t marsbard/tomcat .
