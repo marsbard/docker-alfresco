de150d0c25fe3d349c316cbdfe34e5390dc9539e
Martin Cosgrave <martin@ciderpunk.com>
2015-11-25 20:21:36 +0000

    better skydock startup

--- scripts/run-skydock.sh
+++ scripts/run-skydock.sh
@@ -2,7 +2,7 @@
 
 cd "`dirname $0`"
 
-if docker ps | grep skydock
+if [ "$1" = "stop" ]
 then
 
 	docker stop skydock
@@ -11,16 +11,18 @@ then
 	docker stop skydns
 	docker rm skydns
 
+	exit
 fi
 
+if ! docker ps | grep skydns
+then
+	BRIDGE_IP=$( ip addr | grep -A2 docker0: | grep inet | xargs | cut -f2 -d' ' | cut -f1 -d/ )
 
-BRIDGE_IP=$( ip addr | grep -A2 docker0: | grep inet | xargs | cut -f2 -d' ' | cut -f1 -d/ )
+	docker run -d -p $BRIDGE_IP:53:53/udp --name skydns crosbymichael/skydns -nameserver 8.8.8.8:53 -domain docker
 
-docker run -d -p $BRIDGE_IP:53:53/udp --name skydns crosbymichael/skydns -nameserver 8.8.8.8:53 -domain docker
-
-docker run -d -v /var/run/docker.sock:/docker.sock --name skydock crosbymichael/skydock \
-	-ttl 30 -environment ns -s /docker.sock -domain docker -name skydns
- 
+	docker run -d -v /var/run/docker.sock:/docker.sock --name skydock crosbymichael/skydock \
+		-ttl 30 -environment ns -s /docker.sock -domain docker -name skydns
+fi
 
 
 #cat docker-compose.yml.tmpl | sed "s/<%SKYDNS%>/$BRIDGE_IP/" > docker-compose.yml
