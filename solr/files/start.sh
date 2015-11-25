8f9ef99036ff9c8bcb7c6e8fb940a58d696531fd
Martin Cosgrave <martin@ciderpunk.com>
2015-11-25 11:49:24 +0000

    solr debugging

--- solr/files/start.sh
+++ solr/files/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 # has to happen after the data store is mounted
 if [ ! -f  /data/workspace-SpacesStore/conf/solrconfig.xml ]
 then
@@ -8,6 +10,6 @@ then
 	cp -rv /config/* /data
 fi
 
-
+cp /config/log4j-solr.properties /data
 
 /tomcat/bin/catalina.sh run
