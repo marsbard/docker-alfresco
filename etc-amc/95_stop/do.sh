f0cd5a8bac97928f317dfbb292a7f973063f429d
Martin Cosgrave <martin@ciderpunk.com>
2015-11-26 00:32:42 +0000

    tweaks

--- etc-amc/95_stop/do.sh
+++ etc-amc/95_stop/do.sh
@@ -2,9 +2,10 @@ if [ -f /tmp/amc-pid ]
 then
 
 	PID=$( cat /tmp/amc-pid)
-	echo Killing $PID
+	echo Killing pid $PID
 	kill $PID
 	rm /tmp/amc-pid
 fi
 
+echo Stopping docker images related to alfresco
 docker stop $(docker ps | grep alfresco | awk '{ print $1 }')
