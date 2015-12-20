e2ccedbb1ee2fe246a720d0e8b4e941ba278a2b2
Martin Cosgrave <martin@ciderpunk.com>
2015-12-20 14:14:04 +0000

    got consul working

--- base/files/register-ns.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-MYIP=$( ifconfig eth0 | grep inet\  | cut -f2 -d: | cut -f1 -d' ' )
-HOST=`cat /etc/hostname | cut -f1 -d.`
-curl -XPUT http://${ETCD_HOST}:4001/v2/keys/skydns/docker/ns/${HOST} \
-	-d value='{"Host": "'${MYIP}'"}'
-
-echo "nameserver $ETCD_HOST" > /etc/resolv.conf
