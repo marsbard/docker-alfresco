e4164f578514973c62a7560643121ab2c0d4c033
Martin Cosgrave <martin@ciderpunk.com>
2016-01-13 09:40:41 +0000

    more stuff trying to get httpd proxy to work

--- httpd2/files/generate-certs.sh
+++ httpd2/files/generate-certs.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+if [ ! -f /etc/ssl/openssl.cnf ]
+then
+	cp /etc/ssl.orig/* /etc/ssl
+	rm -rf /etc/ssl.orig
+fi
+
 if [ ! -f /etc/ssl/localhost.cert ]
 then
+	echo Certificate not found, generating
+	ls -l /usr/lib/ssl
+	#export OPENSSL_CONF=/etc/ssl/openssl.cnf
   openssl req  -nodes -new -x509  -keyout /etc/ssl/localhost.key -out /etc/ssl/localhost.cert \
 	  -subj '/CN=localhost/O=Order Of The Bee/C=EU'
 fi
