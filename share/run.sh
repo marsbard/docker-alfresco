945717c6859b98ea15cb87b000683f6899f22142
Martin Cosgrave <martin@ciderpunk.com>
2016-01-20 23:17:53 +0000

    moving getting of artefacts into Dockerfile

--- share/run.sh
+++
@@ -1,9 +0,0 @@
-docker stop -t 120 alfresco-share
-docker rm alfresco-share
-
-docker run -d -p 80:8080 --name alfresco-share marsbard/alfresco-share
-
-sleep 3
-
-docker logs alfresco-share
-
--- solr/run.sh
+++
@@ -1,9 +0,0 @@
-docker stop -t 120 alfresco-solr
-docker rm alfresco-solr
-
-docker run -d -p 80:8080 --name alfresco-solr marsbard/alfresco-solr
-
-sleep 3
-
-docker logs alfresco-solr
-
